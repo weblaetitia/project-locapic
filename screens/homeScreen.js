d2a8c1f9e108a358adea44c4550aa49afda88ce2
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-28 17:12:44 +0200

    integer home screen

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+
+import { FontAwesome } from '@expo/vector-icons'; 
+
+import { Input, Button } from 'react-native-elements';
 
 export default function HomeScreen({navigation}) {
+
   return (
-    <View style={styles.container}>
-      <Text>This is home</Text>
-      <Button
-        title="Go to Map"
-        onPress={() => navigation.navigate('Map')}
+    <ImageBackground source={require('../assets/home.jpg')} style={styles.image}>
+      <Input style={styles.username}
+        containerStyle={{paddingHorizontal: 50}}
+        placeholder='username'
+        leftIcon={
+          <FontAwesome name="user" size={24} color="tomato" />
+        }
       />
-    </View>
+    <Button
+    onPress={() => navigation.navigate('Map')}
+      icon={
+        <FontAwesome name="arrow-right" size={24} color="#fff" style={styles.arrow} />
+      }
+      title="Go to map"
+    />
+    </ImageBackground>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  image: {
+    resizeMode: "cover",
     flex: 1,
-    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, 
+  arrow : {
+    marginRight: 10
+  }
 })
