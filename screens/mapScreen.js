61942ebaa1cf76cc79bc78755a14434420503656
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-29 12:00:37 +0200

    add localisation on map

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,22 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, {Marker} from 'react-native-maps'
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
  function MapScreen({navigation}) {
-  return (
-    <View style={styles.container}>
-        <MapView style={styles.mapStyle}
-                 initialRegion={{
-                  latitude: 48.866667,
-                  longitude: 2.333333,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-                }}
-                >
-        </MapView>
-      </View>
-  );
+
+  const [position, setPosition] = useState(null)
+
+  useEffect(() => {
+    async function askPermisions() {
+      var {status} = await Permissions.askAsync(Permissions.LOCATION)
+      if (status === 'granted') {
+        Location.watchPositionAsync({distanceInterval:2},
+          (location) => {
+            setPosition(location)
+          })
+      }
+    }
+    askPermisions()
+  }, [])
+
+  if (position == null) {
+    return (
+      <View style={styles.container} />
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+          <MapView style={styles.mapStyle}
+                   initialRegion={{
+                    latitude: 48.866667,
+                    longitude: 2.333333,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                  }}
+                  >
+            <Marker coordinate={{ latitude: position.coords.latitude, longitude: position.coords.longitude }}
+                    title='hello'
+                    description='I am here!'
+                    pinColor='pink'
+                    // icon={<FontAwesome5 name="map-marker-alt" size={24} color="red" />}
+                    />          
+          </MapView>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
