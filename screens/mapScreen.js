b55328cf4b9792a0bc577f18872cb777b481dade
Laetitia Langlois <weblaetitia@hotmail.com>
2020-07-29 11:32:03 +0200

    initialise a map on map screen

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,23 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, {Marker} from 'react-native-maps'
 
-
-export default function MapScreen({navigation}) {
+ function MapScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>This is map</Text>
-      <StatusBar style="auto" />
-    </View>
+        <MapView style={styles.mapStyle}
+                 initialRegion={{
+                  latitude: 48.866667,
+                  longitude: 2.333333,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
+                }}
+                >
+        </MapView>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'tomato',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 })
 
+
+
+
+// keep export at the end
+export default MapScreen
